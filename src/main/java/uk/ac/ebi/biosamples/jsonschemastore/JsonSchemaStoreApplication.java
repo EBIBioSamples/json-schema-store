45afbc721389337098410a42faa9b97e3b7d3f3f
hashan <hashan.20@cse.mrt.ac.lk>
2020-05-23 21:21:52 +0530

    Deliverable 1: initial commit

---
+++ src/main/java/uk/ac/ebi/biosamples/jsonschemastore/JsonSchemaStoreApplication.java
@@ -0,0 +1,13 @@
+package uk.ac.ebi.biosamples.jsonschemastore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonSchemaStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonSchemaStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uk/ac/ebi/biosamples/jsonschemastore/JsonSchemaStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.ac.ebi.biosamples.jsonschemastore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonSchemaStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
