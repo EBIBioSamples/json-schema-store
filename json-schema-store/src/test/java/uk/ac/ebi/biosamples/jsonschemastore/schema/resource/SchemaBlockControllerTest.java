727c54d7a78dc85456b65dadb46d187fadae199d
Isuru Liyanage <theisuru@gmail.com>
2020-06-11 07:33:36 +0100

    replace mockMvcTest with springBootTest to test controller without mocking servicese

--- json-schema-store/src/test/java/uk/ac/ebi/biosamples/jsonschemastore/schema/resource/SchemaBlockControllerTest.java
+++ json-schema-store/src/test/java/uk/ac/ebi/biosamples/jsonschemastore/schema/resource/SchemaBlockControllerTest.java
@@ -2,20 +2,20 @@ package uk.ac.ebi.biosamples.jsonschemastore.schema.resource;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@ExtendWith(SpringExtension.class)
-@WebMvcTest(SchemaBlockController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class SchemaBlockControllerTest {
 
-  @Autowired private MockMvc mockMvc;
+  @Autowired
+  private MockMvc mockMvc;
 
   @Test
   public void testGetAllSchemaBlock() throws Exception {
