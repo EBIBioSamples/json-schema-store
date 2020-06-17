bd9d8506117a100ca266760f14cd48b0e34ff1e9
hashan <hashan.20@cse.mrt.ac.lk>
2020-06-17 11:32:11 +0530

    Deliverable 1 : DELETE, PUT endpoints and Unit Tests

--- json-schema-store/src/main/java/uk/ac/ebi/biosamples/jsonschema/jsonschemastore/schema/service/SchemaBlockService.java
+++ json-schema-store/src/main/java/uk/ac/ebi/biosamples/jsonschema/jsonschemastore/schema/service/SchemaBlockService.java
@@ -1,6 +1,9 @@
 package uk.ac.ebi.biosamples.jsonschema.jsonschemastore.schema.service;
 
+import lombok.NonNull;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import uk.ac.ebi.biosamples.jsonschema.jsonschemastore.dto.SchemaBlockDocument;
 import uk.ac.ebi.biosamples.jsonschema.jsonschemastore.schema.document.SchemaBlock;
 import uk.ac.ebi.biosamples.jsonschema.jsonschemastore.schema.repository.SchemaBlockRepository;
 
@@ -10,16 +13,33 @@ import java.util.List;
 public class SchemaBlockService {
 
     private final SchemaBlockRepository schemaBlockRepository;
+    private final ModelMapper modelMapper;
 
-    public SchemaBlockService(SchemaBlockRepository schemaBlockRepository) {
+    public SchemaBlockService(SchemaBlockRepository schemaBlockRepository, ModelMapper modelMapper) {
         this.schemaBlockRepository = schemaBlockRepository;
+        this.modelMapper = modelMapper;
     }
 
-    public SchemaBlock createSchemaBlock(SchemaBlock schemaBlock) {
+    public SchemaBlock createSchemaBlock(@NonNull SchemaBlockDocument schemaBlockDocument) {
+        SchemaBlock schemaBlock = modelMapper.map(schemaBlockDocument, SchemaBlock.class);
         return schemaBlockRepository.save(schemaBlock);
     }
 
     public List<SchemaBlock> getAllSchemaBlocks() {
         return schemaBlockRepository.findAll();
+    }
+
+    public void deleteSchemaBlocks(@NonNull SchemaBlockDocument schemaBlockDocument) {
+        SchemaBlock schemaBlock = modelMapper.map(schemaBlockDocument, SchemaBlock.class);
+        schemaBlockRepository.delete(schemaBlock);
+    }
+
+    public void deleteSchemaBlocksById(@NonNull String id) {
+        schemaBlockRepository.deleteById(id);
+    }
+
+    public void updateSchemaBlocks(SchemaBlockDocument schemaBlockDocument) {
+        //TODO: enable versioning
+        createSchemaBlock(schemaBlockDocument);
     }
 }
