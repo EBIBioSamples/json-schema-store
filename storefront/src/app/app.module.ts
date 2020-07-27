c1508d9bbdb1525575f31b508ce8e971589ba77d
hashan <hashan.20@cse.mrt.ac.lk>
2020-07-28 04:37:57 +0530

    Deliverable 4 : flex layout module and refactor

--- storefront/src/app/app.module.ts
+++ storefront/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 import {AppRoutingModule} from './app-routing.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from './material/material.module';
+import {NgJsonEditorModule} from 'ang-jsoneditor';
+
 import {AppComponent} from './app.component';
 import {SchemaBrowserComponent} from './schema-browser/schema-browser.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {EditorComponent} from './editor/editor.component';
-import {NgJsonEditorModule} from 'ang-jsoneditor';
 import {StoreRoomService} from './service/storeroom/store-room.service';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MaterialModule} from './material/material.module';
 
 @NgModule({
     declarations: [
@@ -21,6 +23,7 @@ import {MaterialModule} from './material/material.module';
     ],
     imports: [
         BrowserModule,
+        FlexLayoutModule,
         BrowserAnimationsModule,
         MaterialModule,
         AppRoutingModule,
