d5e4fd468f74d16d1e3b1abb39e68a3172c66d31
hashan <hashan.20@cse.mrt.ac.lk>
2020-07-31 07:23:45 +0530

    Deliverable 4 : initial routing

--- storefront/src/app/material/json-schema-card/json-schema-card.component.ts
+++ storefront/src/app/material/json-schema-card/json-schema-card.component.ts
@@ -7,7 +7,6 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class JsonSchemaCardComponent implements OnInit {
 
-  // @Input() cardNumber: number;
   @Input() schemaBlock: object;
   constructor() { }
 
