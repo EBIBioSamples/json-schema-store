0d5a7c8f93b0e7044338fa0dfd323c12ee4fb22f
hashan <hashan.20@cse.mrt.ac.lk>
2020-08-07 04:40:45 +0530

    Deliverable 4 : env conf for service

--- storefront/src/app/service/storeroom/store-room.service.ts
+++ storefront/src/app/service/storeroom/store-room.service.ts
@@ -2,38 +2,40 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpResponse} from '@angular/common/http';
 import {Page} from '../../dto/dto.module';
 import {Observable} from 'rxjs';
+import {environment} from '../../../environments/environment';
 
 @Injectable({
     providedIn: 'root'
 })
 export class StoreRoomService {
     public page: Page;
+    private storeroomApi = environment.storeroom_api;
 
     constructor(private http: HttpClient) {
     }
 
     public createJsonSchema(jsonSchema: object): void {
-        this.http.post('http://localhost:8080/api/v1/schemas', jsonSchema)
+        this.http.post(this.storeroomApi + '/schemas', jsonSchema)
             .subscribe((response) => {
                 console.log(response);
             });
     }
 
     public getSchemaBlockPages(pageNumber = 0, pageSize = 3): Observable<Page> {
-        const url = 'http://localhost:8080/api/v1/schemas/page?page=' + pageNumber + '&size=' + pageSize;
+        const url = this.storeroomApi + '/schemas/page?page=' + pageNumber + '&size=' + pageSize;
         return this.http.get<Page>(url);
     }
 
     public getSchemaBlockById(id: string): Observable<any> {
-        const url = 'http://localhost:8080/api/v1/schemas/?id=' + id;
+        const url = this.storeroomApi + '/schemas/?id=' + id;
         return this.http.get(url);
     }
 
     public updateSchemaBlock(jsonSchema: object): Observable<any> {
-        return this.http.put('http://localhost:8080/api/v1/schemas', jsonSchema);
+        return this.http.put(this.storeroomApi + '/schemas', jsonSchema);
     }
 
     public deleteSchemaBlock(id: string): Observable<any> {
-        return this.http.delete('http://localhost:8080/api/v1/schemas/?id=' + id);
+        return this.http.delete(this.storeroomApi + '/schemas/?id=' + id);
     }
 }
