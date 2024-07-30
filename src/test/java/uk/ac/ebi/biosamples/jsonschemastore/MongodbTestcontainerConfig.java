b4d00e80217fd25466ec99ef2d4fd3e612a51464
Isuru Liyanage <theisuru@gmail.com>
2024-07-30 15:44:30 +0100

    replace flapdoodle mongo with testcontainers

---
+++ src/test/java/uk/ac/ebi/biosamples/jsonschemastore/MongodbTestcontainerConfig.java
@@ -0,0 +1,21 @@
+package uk.ac.ebi.biosamples.jsonschemastore;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+
+@Configuration
+@EnableMongoRepositories
+public class MongodbTestcontainerConfig {
+  @Container
+  public static final MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:latest")
+      .withExposedPorts(27017);
+
+  static {
+    mongoDBContainer.start();
+    var mappedPort = mongoDBContainer.getMappedPort(27017);
+    System.setProperty("mongodb.container.port", String.valueOf(mappedPort));
+    System.out.println("======================================= mongo port = " + mappedPort + " =====================");
+  }
+}
