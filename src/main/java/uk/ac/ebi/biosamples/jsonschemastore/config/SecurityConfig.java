89073d78ce4dad7674aee7b39526f2d592594b7c
Isuru Liyanage <theisuru@gmail.com>
2024-12-17 03:56:24 +0000

    allow /schema/list without auth

--- src/main/java/uk/ac/ebi/biosamples/jsonschemastore/config/SecurityConfig.java
+++ src/main/java/uk/ac/ebi/biosamples/jsonschemastore/config/SecurityConfig.java
@@ -65,6 +65,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, basePath + "/mongoJsonSchemas/**").permitAll()
                         .requestMatchers(HttpMethod.GET, basePath + "/fields/**").permitAll()
                         .requestMatchers(HttpMethod.GET,  "/registry/**").permitAll()
+                        .requestMatchers(HttpMethod.GET,  "/api/v2/schemas/list").permitAll()
                         .requestMatchers(HttpMethod.GET,  "/actuator/**").permitAll()
                         .requestMatchers(HttpMethod.GET,  basePath).permitAll()
 
