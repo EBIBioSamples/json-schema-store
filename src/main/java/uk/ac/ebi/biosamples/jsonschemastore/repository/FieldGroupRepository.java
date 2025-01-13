da36c0fd1cea200488a6e1132add11826e52172f
Amnon Khen <amnon@ebi.ac.uk>
2025-01-13 16:08:30 +0000

    ck-185 field groups as a 1st class citizen

--- src/main/java/uk/ac/ebi/biosamples/jsonschemastore/repository/FieldGroupRepository.java
+++ src/main/java/uk/ac/ebi/biosamples/jsonschemastore/repository/FieldGroupRepository.java
@@ -3,6 +3,7 @@ package uk.ac.ebi.biosamples.jsonschemastore.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import uk.ac.ebi.biosamples.jsonschemastore.model.Field;
 import uk.ac.ebi.biosamples.jsonschemastore.model.FieldGroup;
@@ -16,4 +17,11 @@ public interface FieldGroupRepository extends MongoRepository<FieldGroup, String
 
   Page<FieldGroup> findAllByIdIn(List<String> ids, Pageable pageable);
 
+  @Query("{\n" +
+          "    $or: [\n" +
+          "        { \"name\": { $regex: /?0/, $options: \"i\" } },\n" +
+          "        { \"description\": { $regex: /?0/, $options: \"i\" } }\n" +
+          "    ]\n" +
+          "}")
+  Page<FieldGroup> findAllByTextPartial(String text, Pageable pageable);
 }
