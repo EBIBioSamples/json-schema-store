c702660515f68c78e89e2a7a14fcf48281ebbcda
Amnon Khen <amnon@ebi.ac.uk>
2025-02-03 14:12:54 +0000

    ck-86 field text search

---
+++ src/main/java/uk/ac/ebi/biosamples/jsonschemastore/service/FieldEventHandler.java
@@ -0,0 +1,16 @@
+package uk.ac.ebi.biosamples.jsonschemastore.service;
+
+import org.springframework.data.mongodb.core.mapping.event.AbstractMongoEventListener;
+import org.springframework.data.mongodb.core.mapping.event.BeforeSaveEvent;
+import org.springframework.stereotype.Component;
+import uk.ac.ebi.biosamples.jsonschemastore.model.Field;
+import uk.ac.ebi.biosamples.jsonschemastore.model.mongo.MongoJsonSchema;
+
+@Component
+public class FieldEventHandler extends AbstractMongoEventListener<Field> {
+
+    @Override
+    public void onBeforeSave(BeforeSaveEvent<Field> event) {
+        event.getSource().constructTextSearchField();
+    }
+}
