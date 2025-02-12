0aaaea0a9ab562f61091f6b7ce9f3da64d666e1a
Amnon Khen <amnon@ebi.ac.uk>
2025-02-12 19:23:24 +0000

    ck-209: fix schema api links

--- src/main/java/uk/ac/ebi/biosamples/jsonschemastore/controller/RootRedirectController.java
+++ src/main/java/uk/ac/ebi/biosamples/jsonschemastore/controller/RootRedirectController.java
@@ -1,8 +1,10 @@
 package uk.ac.ebi.biosamples.jsonschemastore.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.web.util.RedirectUrlBuilder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
@@ -11,8 +13,11 @@ public class RootRedirectController {
     @Value("${spring.data.rest.basePath}")
     private String apiRootResource;
 
+    @Value("${server.servlet.contextPath:}")
+    private String contextPath;
+
     @GetMapping("/")
     public RedirectView redirectToTarget() {
-        return new RedirectView(apiRootResource);
+        return new RedirectView(contextPath + apiRootResource);
     }
 }
